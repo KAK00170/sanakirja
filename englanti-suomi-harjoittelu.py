2cead7da4388be731b80a305a4376708bc6ff8df
KAK00170 <KAK00170@laurea.fi>
2025-05-23 15:28:43 +0300

    Add files via upload

--- englanti-suomi-harjoittelu.py
+++ englanti-suomi-harjoittelu.py
@@ -1,4 +1,3 @@
-#
 import random
 
 def sanakirja_luku(tiedosto_placeholder):
@@ -10,20 +9,20 @@ def sanakirja_luku(tiedosto_placeholder):
                 sanakirja[englanti] = suomi
     return sanakirja
 
-def käännös(sanakirja, käännöskieli):
+def käännös(sanakirja, käännöskieli):   #Ohjelma
     toistomäärä = 0
     while True:
-        if käännöskieli == "1":
-            sana = random.choice(list(sanakirja.keys()))  #random valinta
+        if käännöskieli == "1":         #Englannista suomeksi
+            sana = random.choice(list(sanakirja.keys()))
             oikea_vastaus = sanakirja[sana]
             vastaus = input(f"Mikä on sanan {sana} suomennos?")
             if vastaus == oikea_vastaus:
                 print("Oikein")
             else:
                 print(f"Väärin, oikea vastaus on: {oikea_vastaus}")
-        elif käännöskieli == "2":
+        elif käännöskieli == "2":       #Suomesta englanniksi
             sana = random.choice(list(sanakirja.values()))
-            oikea_vastaus = [e for e, s in sanakirja.items() if s == sana][0] #e = englanti arvo - s = suomi arvo
+            oikea_vastaus = [e for e, s in sanakirja.items() if s == sana][0]   #e = englanti arvo - s = suomi arvo
             vastaus = input(f"Mikä on sanan {sana} käännös englanniksi? ")
             if vastaus == oikea_vastaus:
                 print("Oikein!")
@@ -33,20 +32,34 @@ def käännös(sanakirja, käännöskieli):
             print("Virheellinen valinta.")
             break
         toistomäärä += 1
-        if toistomäärä % 5 == 0: #Kysytään viiden kysymyksen jälkeen halutaanko jatkaa.
-            jatka = input("Haluatko jatkaa? kyllä/ei: ")
-            if jatka != "kyllä":
-                print("Kiitos!")
-                break
+        if toistomäärä % 5 == 0:        #Kysytään viiden kysymyksen jälkeen halutaanko jatkaa.
+            while True:
+                jatka = input("Haluatko jatkaa? kyllä/ei: ")
+                if jatka == "kyllä":
+                    break
+                elif jatka == "ei":
+                    print("Kiitos!")
+                    return
+                else:
+                    print("Virheellinen valinta. Kirjoita 'kyllä' tai 'ei'")
 
 def main():
     sanakirja = sanakirja_luku("sanakirja.txt")
     
-    print("Valitse käännöskieli:")
-    print("1 = Englannista suomeksi")
-    print("2 = Suomesta englanniksi")
-    käännöskieli = input("Valitse 1 tai 2: ")
-    käännös(sanakirja, käännöskieli)
+    while True:
+        print("Valitse käännöskieli:")
+        print("1 = Englannista suomeksi")
+        print("2 = Suomesta englanniksi")
+        print("0 = Lopeta ohjelma")
+        käännöskieli = input("Valitse 1, 2 tai 0: ")
+        
+        if käännöskieli == "0":
+            print("Kiitos!")
+            break
+        elif käännöskieli == "1" or käännöskieli == "2":
+            käännös(sanakirja, käännöskieli)
+        else:
+            print("Virheellinen valinta, yritä uudelleen.")
 
 if __name__ == "__main__":
     main()
